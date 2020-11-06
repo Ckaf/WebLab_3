8603c47fc4deaac33f5b9ce4f0ccf6a2261e39fb
danila27ru <gorelko.danila@gmail.com>
2020-11-06 03:39:06 +0300

    fix slider pls

--- src/main/java/MainBean.java
+++ src/main/java/MainBean.java
@@ -1,23 +1,21 @@
-
+import lombok.Getter;
+import lombok.Setter;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import java.io.Serializable;
 
-@ManagedBean(name = "MainBean", eager = true)
+@ManagedBean(name = "data", eager = true)
 @ApplicationScoped
+@Getter
+@Setter
 public class MainBean implements Serializable {
     int x;
     int y;
+    int r;
 
-    public void SetParam(int x, int y) {
-        this.x = x;
-        this.y = y;
-        System.out.println(x);
+    public void result(int x, int y, int r) {
+    	//DO SOMETHING
     }
-    public int GetResult(){
-        return x;
-    }
-
 }
