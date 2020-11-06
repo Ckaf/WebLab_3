232f6f7cfadcb1e2458fff713ff5d224e07c1c52
danila_27ru <gorelko.danila@gmail.com>
2020-11-06 21:02:21 +0300

    will manage bean later

---
+++ src/main/java/beans/PointsBean.java
@@ -0,0 +1,28 @@
+package beans;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import java.io.Serializable;
+import java.util.List;
+
+
+@ManagedBean(name = "pointsBean")
+@ApplicationScoped
+@Setter
+public class PointsBean implements Serializable {
+	@Getter
+	private int[] x = new int[9];
+
+	@Getter
+	private double y;
+
+	@Getter
+	private double[] r = new double[12];
+
+	//TODO: finish this
+
+}
