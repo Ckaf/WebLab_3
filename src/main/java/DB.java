7f38b1fcdd82b4c24408bf5d3944a0413c107459
Ckaf <borisnovoselov602@gmail.com>
2020-11-08 03:10:00 +0300

    attempt to pass parameters

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,8 +1,13 @@
+import javax.faces.annotation.ManagedProperty;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
 
 @ManagedBean(name = "DbBean", eager = true)
 @ApplicationScoped
-public class DB {
-    
+public class DB implements Serializable {
+    public void pushInDB(double x, double y, double r, boolean result) {
+        System.out.println(x);
+    }
 }
