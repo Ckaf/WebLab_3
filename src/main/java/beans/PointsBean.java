19efb0d56d9836ce3e5212796d36eccc31a3b8bd
danila_27ru <gorelko.danila@gmail.com>
2020-11-08 18:07:45 +0300

    added DataBaseBean and updated PointsBean

--- src/main/java/beans/PointsBean.java
+++ src/main/java/beans/PointsBean.java
@@ -7,25 +7,42 @@ import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import java.io.Serializable;
-import java.util.List;
-
+import java.util.ArrayList;
 
 @ManagedBean(name = "pointsBean")
 @ApplicationScoped
+@Getter
 @Setter
 public class PointsBean implements Serializable {
-	@Getter
-	private int[] x = new int[9];
+	double x = 0;
+	double y = 0;
+	double r = 1;
+	ArrayList<Double> list = new ArrayList<>();
+	@ManagedProperty(value = "#{dataBaseBean}")
+	DataBaseBean dataBaseBean = new DataBaseBean();
 
-	@Getter
-	private double y;
+	public void submit() {
+		System.out.println("==============================");
+		list.add(x);
+		System.out.println(list);
+		dataBaseBean.addPoint(x, y, r, getResult());
 
-	@Getter
-	private double[] r = new double[12];
+	}
 
-	@Getter
-	private String result;
+	public String getResult() {
+		if (сheckSquare(x, y, r) || сheckQuarterCircle(x, y, r) || сheckTriangle(x, y, r)) return "Y";
+		else return "N";
+	}
 
-	//TODO: finish this
+	private boolean сheckSquare(double x, double y, double r) {
+		return x >= -r && x <= 0 && y >= 0 && y <= r / 2;
+	}
 
+	private boolean сheckQuarterCircle(double x, double y, double r) {
+		return (x <= 0) && (y <= 0) && ((x * x + y * y) <= r * r);
+	}
+
+	private boolean сheckTriangle(double x, double y, double r) {
+		return x >= 0 && x <= r && y >= 0 && y <= r / 2 && (y <= -x / 2 + r / 2);
+	}
 }
