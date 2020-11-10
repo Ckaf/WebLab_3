5ee0bf989f5fbe95bec2ceff4b91e91e66b60415
danila27ru <gorelko.danila@gmail.com>
2020-11-10 06:16:11 +0300

    removed shit

--- src/main/java/Result.java
+++
@@ -1,9 +0,0 @@
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import java.io.Serializable;
-
-@ManagedBean(name = "message", eager = true)
-@RequestScoped
-public class Result implements Serializable {
-
-}
